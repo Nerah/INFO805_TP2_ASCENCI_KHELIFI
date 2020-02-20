725e4b52c945e3b8abdd319c2f600237aaa29917
Nerah <alascenci@gmail.com>
2020-02-20 18:11:37 +0100

    Déplacement structure Background.

--- Renderer.h
+++ Renderer.h
@@ -18,28 +18,6 @@
 
 /// Namespace RayTracer
 namespace rt {
-    struct Background {
-        Color backgroundColor( const Ray& ray ) {
-            Color cInter;
-
-            float z  = ray.direction.at(2);
-            if(z >= 0.0f && z <=0.5f) return Color(1 - 2 * z, 1 - 2 * z,1);
-            if( z > 0.5f && z <= 1.0f) return Color(0, 0, 1.0f - (z - 0.5f) * 2);
-
-            Real x = -0.5f * ray.direction[0] / ray.direction[2];
-            Real y = -0.5f * ray.direction[1] / ray.direction[2];
-            Real d = sqrt(x*x + y*y);
-            Real t = std::min(d, 30.0f) / 30.0f;
-            x -= floor(x);
-            y -= floor(y);
-
-            if ((x >= 0.5f && y >= 0.5f) || (x < 0.5f && y < 0.5f)) cInter = Color( 0.7f, 0.7f, 0.7f );
-            else cInter = Color( 0.9f, 0.9f, 0.9f );
-
-            return (1.0f - t) * cInter + t * Color( 1.0f, 1.0f, 1.0f );
-        }
-    };
-
     inline void progressBar( std::ostream& output,
                              const double currentValue, const double maximumValue)
     {
@@ -71,6 +49,28 @@ namespace rt {
                << " " << (int)(fraction*100)<<"/100\r";
         output.flush();
     }
+
+    struct Background {
+        Color backgroundColor( const Ray& ray ) {
+            Color cInter;
+
+            float z  = ray.direction.at(2);
+            if(z >= 0.0f && z <=0.5f) return Color(1 - 2 * z, 1 - 2 * z,1);
+            if( z > 0.5f && z <= 1.0f) return Color(0, 0, 1.0f - (z - 0.5f) * 2);
+
+            Real x = -0.5f * ray.direction[0] / ray.direction[2];
+            Real y = -0.5f * ray.direction[1] / ray.direction[2];
+            Real d = sqrt(x*x + y*y);
+            Real t = std::min(d, 30.0f) / 30.0f;
+            x -= floor(x);
+            y -= floor(y);
+
+            if ((x >= 0.5f && y >= 0.5f) || (x < 0.5f && y < 0.5f)) cInter = Color( 0.7f, 0.7f, 0.7f );
+            else cInter = Color( 0.9f, 0.9f, 0.9f );
+
+            return (1.0f - t) * cInter + t * Color( 1.0f, 1.0f, 1.0f );
+        }
+    };
 
     /// This structure takes care of rendering a scene.
     struct Renderer {
